39927740a2b9d6545a0da158fd8908a82e9137d7
Deepanshu Sharma <deepanshu123sharma4@gmail.com>
2026-05-07 19:11:54 +0530

    main

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,6 +27,8 @@ app.use(
   })
 );
 
+// main
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/uploads', express.static(uploadsRoot));
